c4fcf143d32f04f2e74b4eab5ab91bdb059e4a94
Katleho14 <katlehomotloung394@gmail.com>
2024-11-08 23:55:34 +0200

    "Updated App.jsx to include routing and sidebar components, removed react.svg asset, and modified main.jsx to use BrowserRouter"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { Route, Routes } from "react-router-dom";
 
+import Sidebar from "./components/common/Sidebar";
 
-
-import './App.css'
+import OverviewPage from "./pages/OverviewPage";
+import ProductsPage from "./pages/ProductsPage";
+import UsersPage from "./pages/UsersPage";
+import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
- 
+	return (
+		<div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+			{/* BG */}
+			<div className='fixed inset-0 z-0'>
+				<div className='absolute inset-0 bg-gradient-to-br from-white via-white to-white opacity-80' />
+				<div className='absolute inset-0 backdrop-blur-sm' />
+			</div>
 
-  return (
-  
-     <div></div> 
-    
-  )
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<OverviewPage />} />
+				<Route path='/products' element={<ProductsPage />} />
+				<Route path='/users' element={<UsersPage />} />
+				<Route path='/sales' element={<SalesPage />} />
+				<Route path='/orders' element={<OrdersPage />} />
+				<Route path='/analytics' element={<AnalyticsPage />} />
+				<Route path='/settings' element={<SettingsPage />} />
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
